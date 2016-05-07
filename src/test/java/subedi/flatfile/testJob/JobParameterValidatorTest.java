de1d3380a6444911a28e4ce9e7450993e2fac6eb
shree665 <shree665@gmail.com>
2016-05-07 13:45:04 -0400

    comments

---
+++ src/test/java/subedi/flatfile/testJob/JobParameterValidatorTest.java
@@ -0,0 +1,26 @@
+package subedi.flatfile.testJob;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JobParameterValidatorTest {
+	
+	String filePath = "classpath:";
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
