fe916f21637da13e4eb1c5d0bba4bb20b04775bf
shree665 <shree665@gmail.com>
2016-05-08 13:54:28 -0400

    adding validator test. still need to do more!

--- src/test/java/subedi/flatfile/testJob/JobParameterValidatorTest.java
+++ src/test/java/subedi/flatfile/testJob/JobParameterValidatorTest.java
@@ -1,21 +1,32 @@
 package subedi.flatfile.testJob;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.hibernate.jpa.criteria.expression.SearchedCaseExpression.WhenClause;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.core.io.ResourceLoader;
 
 public class JobParameterValidatorTest {
 	
-	String filePath = "classpath:";
+	String filePath = "classpath:inst1_awdb.dbo.Agent.20160413.041022.3510025";
+	private ResourceLoader resourceLoader;
+	private ImportValidator validator;
 
 	@Before
 	public void setUp() throws Exception {
+		resourceLoader = mock(ResourceLoader.class, Mockito.RETURNS_DEEP_STUBS);
+		WhenClause(resourceLoader.getResource(filePath));
+		
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
